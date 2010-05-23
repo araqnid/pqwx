844f0a7fea10eb9da8df09614fafa92c9bfa93a1
Steve Haslam <araqnid@googlemail.com>
2010-05-23 16:54:48 +0100

    Add toolbar and new/open menu items

--- querytool_frame.cpp
+++ querytool_frame.cpp
@@ -6,16 +6,24 @@
     #include "wx/wx.h"
 #endif
 
+#include "wx/toolbar.h"
+
 #include "querytool_frame.h"
 
 #if !defined(__WXMSW__) && !defined(__WXPM__)
     #include "querytool-appicon.xpm"
 #endif
 
+#include "toolbar-new.xpm"
+
 BEGIN_EVENT_TABLE(QueryToolFrame, wxFrame)
   EVT_MENU(QueryTool_Quit, QueryToolFrame::OnQuit)
   EVT_MENU(QueryTool_About, QueryToolFrame::OnAbout)
+  EVT_MENU(QueryTool_New, QueryToolFrame::OnNew)
+  EVT_MENU(QueryTool_Open, QueryToolFrame::OnOpen)
 END_EVENT_TABLE()
+
+const int TOOLBAR_MAIN = 500;
 
 QueryToolFrame::QueryToolFrame(const wxString& title)
   : wxFrame(NULL, wxID_ANY, title)
@@ -23,6 +31,8 @@ QueryToolFrame::QueryToolFrame(const wxString& title)
   SetIcon(wxICON(querytool_appicon));
 
   wxMenu *fileMenu = new wxMenu;
+  fileMenu->Append(QueryTool_New, _T("&New query\tCtrl-N"), _T("Create a new query"));
+  fileMenu->Append(QueryTool_Open, _T("&Open script\tCtrl-O"), _T("Open a SQL script"));
   fileMenu->Append(QueryTool_Quit, _T("E&xit\tCtrl-Q"), _T("Exit Query Tool"));
 
   wxMenu *helpMenu = new wxMenu;
@@ -33,6 +43,13 @@ QueryToolFrame::QueryToolFrame(const wxString& title)
   menuBar->Append(helpMenu, _T("&Help"));
 
   SetMenuBar(menuBar);
+
+  CreateToolBar(wxNO_BORDER | wxHORIZONTAL | wxTB_FLAT, TOOLBAR_MAIN);
+  GetToolBar()->SetMargins(2, 2);
+  GetToolBar()->SetToolBitmapSize(wxSize(16, 15));
+  GetToolBar()->AddTool(wxID_NEW, _T("NEW"), wxBitmap(new_xpm), wxNullBitmap, wxITEM_NORMAL,
+			_T("New query"), _T("Create a new query"));
+  GetToolBar()->Realize();
 
   CreateStatusBar(2);
 }
@@ -55,4 +72,14 @@ void QueryToolFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
 	       wxOK | wxICON_INFORMATION,
 	       this
 	       );
+}
+
+void QueryToolFrame::OnNew(wxCommandEvent& event)
+{
+  wxLogStatus(_T("Create new script here..."));
+}
+
+void QueryToolFrame::OnOpen(wxCommandEvent& event)
+{
+  wxLogStatus(_T("Open script file here..."));
 }
--- querytool_frame.h
+++ querytool_frame.h
@@ -3,7 +3,9 @@ public:
   QueryToolFrame(const wxString& title);
 
   void OnQuit(wxCommandEvent& event);
-  void OnAbout(wxCommandEvent& evet);
+  void OnAbout(wxCommandEvent& event);
+  void OnNew(wxCommandEvent& event);
+  void OnOpen(wxCommandEvent& event);
 
 private:
   DECLARE_EVENT_TABLE();
@@ -12,6 +14,8 @@ private:
 // controls and menu commands
 enum {
   QueryTool_Quit = wxID_EXIT,
-  QueryTool_About = wxID_ABOUT
+  QueryTool_About = wxID_ABOUT,
+  QueryTool_New = wxID_NEW,
+  QueryTool_Open = wxID_OPEN
 };
 

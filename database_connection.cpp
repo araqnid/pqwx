69178e2c9bd2872b34442065056fa36561056807
Steve Haslam <araqnid@googlemail.com>
2011-01-16 03:12:28 +0000

    Perform database interaction in a separate worker thread.
    DatabaseConnection provides methods for work to be performed asynchronously,
    calling a completion port when done. The existing methods perform work
    synchronously, although all work is funnelled through the worker thread
    (so may be queued).

--- database_connection.cpp
+++ database_connection.cpp
@@ -6,18 +6,64 @@
 using namespace std;
 
 void DatabaseConnection::setup() {
-  ExecCommand("SET client_encoding TO 'UTF8'");
+  wxCriticalSectionLocker enter(workerThreadPointer);
+  workerThread = new DatabaseWorkerThread(this);
+  workerThread->Create();
+  workerThread->Run();
+  connected = 1;
+  AddWork(new DatabaseCommandWork("SET client_encoding TO 'UTF8'"));
+  AddWork(new DatabaseCommandWork("SET DateStyle = 'ISO'"));
 }
 
 void DatabaseConnection::dispose() {
   if (!connected)
     return;
 
-  PQfinish(conn);
   connected = 0;
+
+  wxCriticalSectionLocker enter(executing);
+  PQfinish(conn);
 }
 
 bool DatabaseConnection::ExecQuery(const char *sql, vector< vector<wxString> >& results) {
+  if (!connected) {
+    return ExecQuerySync(sql, results);
+  }
+
+  DatabaseWork *work = new DatabaseQueryWork(sql, &results);
+  AddWork(work);
+  work->await();
+  return work->getResult();
+}
+
+bool DatabaseConnection::ExecCommand(const char *sql) {
+  if (!connected) {
+    return ExecCommandSync(sql);
+  }
+
+  DatabaseWork *work = new DatabaseCommandWork(sql);
+  AddWork(work);
+  work->await();
+  return work->getResult();
+}
+
+void DatabaseConnection::ExecQueryAsync(const char *sql, vector< vector<wxString> >& results, DatabaseWorkCompletionPort *completionPort) {
+  AddWork(new DatabaseQueryWork(sql, &results, completionPort));
+}
+
+void DatabaseConnection::ExecCommandAsync(const char *sql, DatabaseWorkCompletionPort *completionPort) {
+  AddWork(new DatabaseCommandWork(sql, completionPort));
+}
+
+void DatabaseConnection::ExecCommandsAsync(vector<const char *> sql, DatabaseWorkCompletionPort *completionPort) {
+  for (vector<const char *>::iterator iter = sql.begin(); iter != sql.end(); iter++) {
+    AddWork(new DatabaseCommandWork(*iter, completionPort));
+  }
+}
+
+bool DatabaseConnection::ExecQuerySync(const char *sql, vector< vector<wxString> >& results) {
+  wxCriticalSectionLocker locker(executing);
+
   PGresult *rs = PQexec(conn, sql);
   if (!rs)
     return false;
@@ -42,7 +88,9 @@ bool DatabaseConnection::ExecQuery(const char *sql, vector< vector<wxString> >& 
   return true;
 }
 
-bool DatabaseConnection::ExecCommand(const char *sql) {
+bool DatabaseConnection::ExecCommandSync(const char *sql) {
+  wxCriticalSectionLocker locker(executing);
+
   PGresult *rs = PQexec(conn, sql);
   if (!rs)
     return false;
@@ -54,4 +102,28 @@ bool DatabaseConnection::ExecCommand(const char *sql) {
   PQclear(rs);
 
   return true;
+}
+
+wxThread::ExitCode DatabaseWorkerThread::Entry() {
+  db->workConditionMutex.Lock();
+
+  do {
+    if (finish) break;
+    for (vector<DatabaseWork*>::iterator iter = work.begin(); iter != work.end(); iter++) {
+      DatabaseWork *work = *iter;
+      bool result = work->execute(db);
+      work->finished(result);
+    }
+    work.clear();
+    db->workCondition.Wait();
+  } while (true);
+}
+
+void DatabaseConnection::AddWork(DatabaseWork *work) {
+  wxMutexLocker locker(workConditionMutex);
+  if (work)
+    workerThread->work.push_back(work);
+  else
+    workerThread->finish = true;
+  workCondition.Signal();
 }
--- database_connection.h
+++ database_connection.h
@@ -1,15 +1,26 @@
+// -*- c++ -*-
+
 #ifndef __database_connection_h
 #define __database_connection_h
 
 #include "wx/string.h"
+#include "wx/thread.h"
 #include "server_connection.h"
+
+class DatabaseWorkerThread;
+class DatabaseWork;
+
+class DatabaseWorkCompletionPort {
+public:
+  virtual void complete(bool result) = 0;
+};
 
 class DatabaseConnection {
 public:
-  DatabaseConnection(ServerConnection *server_, PGconn *conn_) {
+  DatabaseConnection(ServerConnection *server_, PGconn *conn_) : workCondition(workConditionMutex) {
     server = server_;
-    connected = 1;
     conn = conn_;
+    connected = 0;
     setup();
   }
 
@@ -21,12 +32,99 @@ public:
 
   bool ExecQuery(const char *sql, std::vector< std::vector<wxString> >& results);
   bool ExecCommand(const char *sql);
-
 private:
   void setup();
   PGconn *conn;
   ServerConnection *server;
   int connected;
+  void AddWork(DatabaseWork*);
+public:
+  void ExecQueryAsync(const char *sql, std::vector< std::vector<wxString> >& results, DatabaseWorkCompletionPort *completion);
+  void ExecCommandAsync(const char *sql, DatabaseWorkCompletionPort *completion);
+  void ExecCommandsAsync(std::vector<const char *> sql, DatabaseWorkCompletionPort *completion);
+  bool ExecQuerySync(const char *sql, std::vector< std::vector<wxString> >& results);
+  bool ExecCommandSync(const char *sql);
+public:
+  DatabaseWorkerThread *workerThread;
+  wxCriticalSection executing;
+  wxCriticalSection workerThreadPointer;
+  wxMutex workConditionMutex;
+  wxCondition workCondition;
+};
+
+class DatabaseWorkerThread : public wxThread {
+public:
+  DatabaseWorkerThread(DatabaseConnection *db) : db(db), wxThread(wxTHREAD_DETACHED) {
+    finish = false;
+  }
+
+  ~DatabaseWorkerThread() {
+    wxCriticalSectionLocker enter(db->workerThreadPointer);
+    db->workerThread = NULL;
+  }
+
+  std::vector<DatabaseWork*> work;
+  bool finish;
+
+protected:
+  virtual ExitCode Entry();
+
+private:
+  DatabaseConnection *db;
+};
+
+class DatabaseWork {
+public:
+  DatabaseWork(DatabaseWorkCompletionPort *completion = NULL) : condition(mutex), done(false), completion(completion) {}
+  virtual bool execute(DatabaseConnection *db) = 0;
+  void await() {
+    wxMutexLocker locker(mutex);
+    do {
+      if (done)
+	return;
+      condition.Wait();
+    } while (true);
+  }
+  void finished(bool result_) {
+    wxMutexLocker locker(mutex);
+    done = true;
+    result = result_;
+    condition.Signal();
+  }
+  bool getResult() {
+    wxMutexLocker locker(mutex);
+    if (!done)
+      fprintf(stderr, "requesting work result when it isn't finished!\n");
+    return result;
+  }
+private:
+  DatabaseWorkCompletionPort *completion;
+  wxMutex mutex;
+  wxCondition condition;
+  DatabaseConnection *db;
+  bool done;
+  bool result;
+};
+
+class DatabaseQueryWork : public DatabaseWork {
+public:
+  DatabaseQueryWork(const char *sql, std::vector< std::vector<wxString> > *results, DatabaseWorkCompletionPort *completion = NULL) : DatabaseWork(completion), sql(sql), results(results) {}
+  bool execute(DatabaseConnection *db) {
+    return db->ExecQuerySync(sql, *results);
+  }
+private:
+  std::vector< std::vector<wxString> > *results;
+  const char *sql;
+};
+
+class DatabaseCommandWork : public DatabaseWork {
+public:
+  DatabaseCommandWork(const char *sql, DatabaseWorkCompletionPort *completion = NULL) : DatabaseWork(completion), sql(sql) {}
+  bool execute(DatabaseConnection *db) {
+    return db->ExecCommandSync(sql);
+  }
+private:
+  const char *sql;
 };
 
 #endif

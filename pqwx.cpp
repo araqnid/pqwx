b360ab64de5df7372345db2beb2f0fcfbb9112aa
Steve Haslam <araqnid@googlemail.com>
2011-12-14 20:33:58 +0000

    Support taking parameters for initial connection from command line

--- pqwx.cpp
+++ pqwx.cpp
@@ -6,13 +6,17 @@
     #include "wx/wx.h"
 #endif
 
+#include "wx/cmdline.h"
 #include "pqwx_frame.h"
-
 #include "server_connection.h"
 
 class PQWXApp: public wxApp {
 public:
   virtual bool OnInit();
+  void OnInitCmdLine(wxCmdLineParser &parser);
+  bool OnCmdLineParsed(wxCmdLineParser &parser);
+private:
+  ServerConnection *initialConnection;
 };
 
 IMPLEMENT_APP(PQWXApp)
@@ -25,9 +29,55 @@ bool PQWXApp::OnInit()
   PqwxFrame *frame = new PqwxFrame(_T("PQWX"));
   frame->Show(true);
 
-  ServerConnection *conn = new ServerConnection();
-  frame->objectBrowser->AddServerConnection(conn);
+  if (initialConnection)
+    frame->objectBrowser->AddServerConnection(initialConnection);
 
   return true;
 }
 
+void PQWXApp::OnInitCmdLine(wxCmdLineParser &parser) {
+  parser.AddOption(_T("S"), _T("server"), _("server host (or host:port)"));
+  parser.AddOption(_T("U"), _T("user"), _("login username"));
+  parser.AddOption(_T("P"), _T("password"), _("login password"));
+  wxApp::OnInitCmdLine(parser);
+}
+
+bool PQWXApp::OnCmdLineParsed(wxCmdLineParser &parser) {
+  wxString server, user, password;
+  bool haveInitial = false;
+
+  if (parser.Found(_T("S"), &server)) {
+    haveInitial = true;
+  }
+  if (parser.Found(_T("U"), &user)) {
+    haveInitial = true;
+  }
+  if (parser.Found(_T("P"), &password)) {
+    haveInitial = true;
+  }
+
+  if (haveInitial) {
+    initialConnection = new ServerConnection();
+    if (!server.IsEmpty()) {
+      int colon = server.Find(_T(':'));
+      if (colon == wxNOT_FOUND) {
+	initialConnection->hostname = strdup(server.utf8_str());
+      }
+      else {
+	initialConnection->hostname = strdup(server.Mid(0, colon).utf8_str());
+	initialConnection->port = atoi(server.Mid(colon+1).utf8_str());
+      }
+    }
+    if (!user.IsEmpty()) {
+      initialConnection->username = strdup(user.utf8_str());
+    }
+    if (!password.IsEmpty()) {
+      initialConnection->password = strdup(password.utf8_str());
+    }
+  }
+  else {
+    initialConnection = NULL;
+  }
+
+  return true;
+}

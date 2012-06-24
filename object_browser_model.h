725aade37845c40017e80aa761e873a69689d3b9
Steve Haslam <araqnid@googlemail.com>
2012-06-24 18:04:10 +0100

    Store serverId as a string in object model references rather than an integer.
    This lines up with how FindServer actually works currently, and drops
    the need for obsolete server models to be somehow kept in a vector.

--- object_browser_model.h
+++ object_browser_model.h
@@ -413,9 +413,9 @@ inline DatabaseConnection *DatabaseModel::GetDatabaseConnection() {
  */
 class ObjectModelReference {
 public:
-  ObjectModelReference(unsigned serverId) : serverId(serverId), database(InvalidOid), regclass(InvalidOid), oid(InvalidOid), subid(0) {}
-  ObjectModelReference(unsigned serverId, Oid database) : serverId(serverId), database(database), regclass(1262), oid(database), subid(0) {}
-  ObjectModelReference(unsigned serverId, Oid regclass, Oid oid) : serverId(serverId), database(InvalidOid), regclass(regclass), oid(oid), subid(0) {}
+  ObjectModelReference(const wxString& serverId) : serverId(serverId), database(InvalidOid), regclass(InvalidOid), oid(InvalidOid), subid(0) {}
+  ObjectModelReference(const wxString& serverId, Oid database) : serverId(serverId), database(database), regclass(1262), oid(database), subid(0) {}
+  ObjectModelReference(const wxString& serverId, Oid regclass, Oid oid) : serverId(serverId), database(InvalidOid), regclass(regclass), oid(oid), subid(0) {}
   ObjectModelReference(const ObjectModelReference& database, Oid regclass, Oid oid, int subid = 0) : serverId(database.serverId), database(database.oid), regclass(regclass), oid(oid), subid(subid) {}
 
   wxString Identify() const
@@ -433,14 +433,14 @@ public:
     return buf;
   }
 
-  unsigned GetServerId() const { return serverId; }
+  wxString GetServerId() const { return serverId; }
   Oid GetDatabase() const { return database; }
   Oid GetObjectClass() const { return regclass; }
   Oid GetOid() const { return oid; }
   int GetObjectSubid() const { return subid; }
 
 private:
-  unsigned serverId;
+  wxString serverId;
   Oid database;
   Oid regclass;
   Oid oid;
